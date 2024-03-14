5721994f44ad597946dbd188b84682385eca14db
tan <13320385140@163.com>
2024-03-15 00:10:58 +0800

    处理 Map<String,Bean> 中的Bean类型丢失

--- trpc-core/src/main/java/cn/tannn/trpc/core/consumer/TInvocationHandler.java
+++ trpc-core/src/main/java/cn/tannn/trpc/core/consumer/TInvocationHandler.java
@@ -9,9 +9,8 @@ import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import okhttp3.*;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -47,11 +46,43 @@ public class TInvocationHandler implements InvocationHandler {
         // 发送请
         RpcResponse rpcResponse = post(rpcRequest);
         if (rpcResponse.isStatus()) {
-            // 处理结果类型
-//            return JSON.to(method.getReturnType(),  rpcResponse.getData());
-            // TypeUtils.cast 是模拟上面那个写的
-            return TypeUtils.cast( rpcResponse.getData(), method.getReturnType());
-
+            Object data = rpcResponse.getData();
+            Class<?> returnType = method.getReturnType();
+            // 处理 Map<String,Bean> 中的Bean类型丢失
+            if (data instanceof JSONObject jsonResult && Map.class.isAssignableFrom(returnType)) {
+                Type genericReturnType = method.getGenericReturnType();
+                Map resultMap = new HashMap<>();
+                if (genericReturnType instanceof ParameterizedType parameterizedType) {
+                    Type actualTypeKey = parameterizedType.getActualTypeArguments()[0];
+                    Type actualTypeValue = parameterizedType.getActualTypeArguments()[1];
+                    jsonResult.forEach((k,v) -> {
+                        resultMap.put(TypeUtils.cast(k, (Class<?>) actualTypeKey),
+                                TypeUtils.cast(v, (Class<?>) actualTypeValue));
+                    });
+                } else {
+                     jsonResult.toJavaObject(method.getReturnType());
+                }
+                return resultMap;
+            }else if (data instanceof JSONArray jsonArray && List.class.isAssignableFrom(returnType)) {
+                //  处理 List<Bean> 中的Bean类型丢失
+                Object[] array = jsonArray.toArray();
+                List<Object> resultList = new ArrayList<>(array.length);
+                Type genericReturnType = method.getGenericReturnType();
+                if (genericReturnType instanceof ParameterizedType parameterizedType) {
+                    Type actualType = parameterizedType.getActualTypeArguments()[0];
+                    for (Object o : array) {
+                        resultList.add(TypeUtils.cast(o, (Class<?>) actualType));
+                    }
+                } else {
+                    resultList.addAll(Arrays.asList(array));
+                }
+                return resultList;
+            }else {
+                // 处理结果类型
+//            return JSON.to(method.getReturnType(),  data);
+                // TypeUtils.cast 是模拟上面那个写的
+                return TypeUtils.cast(data, method.getReturnType());
+            }
         } else {
             // 处理回传的调用期间发生的异常
             Exception ex = rpcResponse.getEx();
