284d6681d5f324bfc482e0dd3a4733d174d32835
tan <13320385140@163.com>
2024-03-22 23:40:37 +0800

    fix: test run 中请求自己不通过的问题

--- trpc-demo/trpc-demo-consumer/src/test/java/cn/tannn/trpc/demo/consumer/TestTrpcDemoConsumerApplication.java
+++ trpc-demo/trpc-demo-consumer/src/test/java/cn/tannn/trpc/demo/consumer/TestTrpcDemoConsumerApplication.java
@@ -3,15 +3,24 @@ package cn.tannn.trpc.demo.consumer;
 
 import cn.tannn.trpc.core.annotation.EnableConsumer;
 import cn.tannn.trpc.core.annotation.EnableProvider;
+import cn.tannn.trpc.demo.consumer.runner.StartUpCheck;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 
 /**
  * @author tnnn
  */
 @SpringBootApplication
-@EnableConsumer
-@EnableProvider
 public class TestTrpcDemoConsumerApplication {
-
-
+    @Bean
+    public ApplicationRunner startUpCheck() {
+        return args -> {
+            // This ApplicationRunner will only be included in tests
+        };
+    }
 }
--- trpc-demo/trpc-demo-consumer/src/test/java/cn/tannn/trpc/demo/consumer/TrpcDemoConsumerApplicationTests.java
+++ trpc-demo/trpc-demo-consumer/src/test/java/cn/tannn/trpc/demo/consumer/TrpcDemoConsumerApplicationTests.java
@@ -3,7 +3,8 @@ package cn.tannn.trpc.demo.consumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+@SpringBootTest(
+//        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = TestTrpcDemoConsumerApplication.class)
 class TrpcDemoConsumerApplicationTests {
 
