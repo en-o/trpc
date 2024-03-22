ba541e36eaf19aee9993894b4ef0c8b03cb6ceb6
en-o <13320385140@163.com>
2024-03-22 15:36:31 +0800

    修改测试用例

--- trpc-demo/trpc-demo-provider/src/main/java/cn/tannn/trpc/demo/provider/OrderServiceImpl.java
+++ trpc-demo/trpc-demo-provider/src/main/java/cn/tannn/trpc/demo/provider/OrderServiceImpl.java
@@ -3,6 +3,8 @@ package cn.tannn.trpc.demo.provider;
 import cn.tannn.trpc.core.annotation.TProvider;
 import cn.tannn.trpc.demo.api.OrderService;
 import cn.tannn.trpc.demo.api.entity.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,17 +17,24 @@ import org.springframework.stereotype.Component;
 @Component
 @TProvider
 public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    Environment environment;
+
+
     @Override
     public Order findById(Integer id) {
         if(id == 404){
             throw new RuntimeException("404了呀");
         }
-        return new Order(id.longValue(), 15.6f);
+        String property = environment.getProperty("server.port");
+        return new Order(id.longValue(), Float.valueOf(property));
     }
 
     @Override
     public Order findById2(Long id) {
-        return new Order(id, 15.6f);
+        String property = environment.getProperty("server.port");
+        return new Order(id,  Float.valueOf(property));
     }
 
     @Override
