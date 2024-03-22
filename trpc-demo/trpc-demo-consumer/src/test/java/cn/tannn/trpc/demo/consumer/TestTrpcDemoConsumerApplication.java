a9dabae7c86a146ee7d2c996435360d88847d965
tan <13320385140@163.com>
2024-03-22 22:11:50 +0800

    新增测试类

---
+++ trpc-demo/trpc-demo-consumer/src/test/java/cn/tannn/trpc/demo/consumer/TestTrpcDemoConsumerApplication.java
@@ -0,0 +1,18 @@
+
+package cn.tannn.trpc.demo.consumer;
+
+import cn.tannn.trpc.core.annotation.EnableConsumer;
+import cn.tannn.trpc.core.annotation.EnableProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tnnn
+ */
+@SpringBootApplication
+@EnableConsumer
+@EnableProvider
+public class TestTrpcDemoConsumerApplication {
+
+
+}
--- trpc-demo/trpc-demo-consumer/src/test/java/cn/tannn/trpc/demo/consumer/TrpcDemoConsumerApplicationTests.java
+++ trpc-demo/trpc-demo-consumer/src/test/java/cn/tannn/trpc/demo/consumer/TrpcDemoConsumerApplicationTests.java
@@ -1,9 +1,11 @@
 package cn.tannn.trpc.demo.consumer;
 
+import cn.tannn.trpc.core.annotation.EnableProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = TestTrpcDemoConsumerApplication.class)
 class TrpcDemoConsumerApplicationTests {
 
     @Test
