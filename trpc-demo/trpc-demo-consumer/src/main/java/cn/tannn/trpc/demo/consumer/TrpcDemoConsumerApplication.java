62459b763dc864a5de1fb9277e25e33d77968c85
tan <13320385140@163.com>
2024-03-22 21:31:37 +0800

    删除多余代码

--- trpc-demo/trpc-demo-consumer/src/main/java/cn/tannn/trpc/demo/consumer/TrpcDemoConsumerApplication.java
+++ trpc-demo/trpc-demo-consumer/src/main/java/cn/tannn/trpc/demo/consumer/TrpcDemoConsumerApplication.java
@@ -2,10 +2,8 @@
 package cn.tannn.trpc.demo.consumer;
 
 import cn.tannn.trpc.core.annotation.EnableConsumer;
-import cn.tannn.trpc.demo.consumer.runner.StartUpCheck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author tnnn
@@ -19,13 +17,5 @@ public class TrpcDemoConsumerApplication {
         SpringApplication.run(TrpcDemoConsumerApplication.class, args);
     }
 
-
-    /**
-     * 启动测试用的
-     */
-    @Bean
-    public StartUpCheck startUpCheck() {
-        return new StartUpCheck();
-    }
 
 }
