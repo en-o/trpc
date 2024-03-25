b6b1324697203da535ce448c7b8f6ffd540b3f86
en-o <13320385140@163.com>
2024-03-25 09:31:29 +0800

    RPC ZK  MOCK

--- trpc-demo/trpc-demo-provider/src/test/java/cn/tannn/trpc/demo/provider/TrpcDemoProviderApplicationTests.java
+++ trpc-demo/trpc-demo-provider/src/test/java/cn/tannn/trpc/demo/provider/TrpcDemoProviderApplicationTests.java
@@ -1,13 +1,26 @@
 package cn.tannn.trpc.demo.provider;
 
+import cn.tannn.trpc.core.test.TestZKServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TrpcDemoProviderApplicationTests {
+    static TestZKServer zkServer = new TestZKServer();
+    @BeforeAll
+    static void init() {
+        zkServer.start();
+    }
 
     @Test
     void contextLoads() {
+        System.out.println(" ===> trpcDemoProviderApplicationTests  .... ");
     }
 
+    @AfterAll
+    static void destory() {
+        zkServer.stop();
+    }
 }
