0c328b3b02b2e4f33fc2daf0b4389b772aad46e0
tan <13320385140@163.com>
2024-03-31 23:18:43 +0800

    主分支重写

--- trpc-demo/trpc-demo-consumer/src/main/java/cn/tannn/trpc/demo/consumer/TrpcDemoConsumerApplication.java
+++ trpc-demo/trpc-demo-consumer/src/main/java/cn/tannn/trpc/demo/consumer/TrpcDemoConsumerApplication.java
@@ -1,31 +1,17 @@
 
 package cn.tannn.trpc.demo.consumer;
 
-import cn.tannn.trpc.core.annotation.EnableConsumer;
-import cn.tannn.trpc.demo.consumer.runner.StartUpCheck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author tnnn
  */
 @SpringBootApplication
-@EnableConsumer
 public class TrpcDemoConsumerApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(TrpcDemoConsumerApplication.class, args);
-    }
-
-
-    /**
-     * 启动测试用的
-     */
-    @Bean
-    public StartUpCheck startUpCheck() {
-        return new StartUpCheck();
     }
 
 }
--- trpc-demo/trpc-demo-provider/src/main/java/cn/tannn/trpc/demo/provider/TrpcDemoProviderApplication.java
+++ trpc-demo/trpc-demo-provider/src/main/java/cn/tannn/trpc/demo/provider/TrpcDemoProviderApplication.java
@@ -1,18 +1,7 @@
 package cn.tannn.trpc.demo.provider;
 
-import cn.tannn.trpc.core.annotation.EnableProvider;
-import cn.tannn.trpc.core.api.RpcRequest;
-import cn.tannn.trpc.core.api.RpcResponse;
-import cn.tannn.trpc.core.providers.ProviderBootstrap;
-import cn.tannn.trpc.core.util.MethodUtils;
-import cn.tannn.trpc.demo.api.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,43 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @SpringBootApplication
-@EnableProvider
 public class TrpcDemoProviderApplication {
 
-    @Autowired
-    private ProviderBootstrap providerBootstrap;
 
     public static void main(String[] args) {
         SpringApplication.run(TrpcDemoProviderApplication.class, args);
     }
 
 
-    /**
-     * 使用 HTTP + JSON 来实现序列化和通信
-     *
-     * @param request 需要调用的接口信息 （通过接口信息去调用接口，然后返回接口执行结果
-     * @return RpcResponse
-     */
-    @RequestMapping("/")
-    public RpcResponse invoke(@RequestBody RpcRequest request) {
-        return providerBootstrap.invokeRequest(request);
-    }
-
-
-    /**
-     * 启动 模拟调用一次请求测试下 （接口测试：demo.http）
-     * @return ApplicationRunner
-     */
-    @Bean
-    public ApplicationRunner provideRun(){
-        return x -> {
-            RpcRequest rpcRequest = new RpcRequest();
-            rpcRequest.setService("cn.tannn.trpc.demo.api.UserService");
-            rpcRequest.setMethodSign(MethodUtils.methodSign(UserService.class.getMethod("findById",Integer.class)));
-            rpcRequest.setArgs(new Object[]{100});
-            RpcResponse rpcResponse = providerBootstrap.invokeRequest(rpcRequest);
-            System.out.println("return: " + rpcResponse.getData());
-        };
-    }
 
 }
