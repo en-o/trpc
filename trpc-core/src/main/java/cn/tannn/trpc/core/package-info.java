1f7d9b658f9ff2f77a791da833e3fa74a8266b71
tan <13320385140@163.com>
2024-03-31 23:26:25 +0800

    主分支重写

---
+++ trpc-core/src/main/java/cn/tannn/trpc/core/package-info.java
@@ -0,0 +1,1 @@
+package cn.tannn.trpc.core;
--- trpc-demo/trpc-demo-provider/src/test/java/cn/tannn/trpc/demo/provider/TrpcDemoProviderApplicationTests.java
+++ trpc-demo/trpc-demo-provider/src/test/java/cn/tannn/trpc/demo/provider/TrpcDemoProviderApplicationTests.java
@@ -1,31 +1,17 @@
 package cn.tannn.trpc.demo.provider;
 
-import cn.tannn.trpc.core.test.TestZKServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TrpcDemoProviderApplicationTests {
-    static TestZKServer zkServer = new TestZKServer();
-    @BeforeAll
-    static void init() {
-        System.out.println(" ====================================== ");
-        System.out.println(" ====================================== ");
-        System.out.println(" =============     ZK2182    ========== ");
-        System.out.println(" ====================================== ");
-        System.out.println(" ====================================== ");
-        zkServer.start();
-    }
+
 
     @Test
     void contextLoads() {
         System.out.println(" ===> trpcDemoProviderApplicationTests  .... ");
     }
 
-    @AfterAll
-    static void destory() {
-        zkServer.stop();
-    }
+
 }
