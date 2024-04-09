b9a457615ca044cd404fc41789a74a471eed6b72
tan <13320385140@163.com>
2024-04-09 19:38:09 +0800

    初步nacos注册成功

--- trpc-registry/trpc-registry-nacos/src/main/java/cn/tannn/trpc/registry/nacos/NacosRegistryCenter.java
+++ trpc-registry/trpc-registry-nacos/src/main/java/cn/tannn/trpc/registry/nacos/NacosRegistryCenter.java
@@ -1,22 +1,31 @@
 package cn.tannn.trpc.registry.nacos;
 
 import cn.tannn.trpc.common.api.RegistryCenter;
+import cn.tannn.trpc.common.exception.ExceptionCode;
 import cn.tannn.trpc.common.exception.TrpcException;
 import cn.tannn.trpc.common.listener.ChangedListener;
+import cn.tannn.trpc.common.listener.Event;
 import cn.tannn.trpc.common.meta.InstanceMeta;
 import cn.tannn.trpc.common.meta.ServiceMeta;
+import cn.tannn.trpc.common.properties.meta.GrayMetas;
+import cn.tannn.trpc.common.properties.rc.Connect;
 import cn.tannn.trpc.common.properties.rc.RegistryCenterProperties;
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.nacos.api.naming.NamingFactory;
 import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.listener.NamingEvent;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.alibaba.nacos.common.utils.MapUtil;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  * nacos注册中心
@@ -28,47 +37,90 @@ import java.util.Map;
 public class NacosRegistryCenter implements RegistryCenter {
 
     private final RegistryCenterProperties rcp;
+    private NamingService client;
 
     public NacosRegistryCenter(RegistryCenterProperties rcp) {
         this.rcp = rcp;
-        throw new TrpcException("暂不支持nacos");
     }
 
+    @SneakyThrows
     @Override
     public void start() {
-
+        if (client != null) {
+            log.info("nacosServer alive ... ");
+            return;
+        }
+        Connect[] connect = rcp.getConnect();
+        // @see com.alibaba.nacos.api.PropertyKeyConst
+        Properties properties = new Properties();
+        if (connect == null || connect.length == 0) {
+            // "请填写注册中心连接信息"
+            throw new TrpcException(ExceptionCode.ZK_NOT_SETTING);
+        } else {
+            // todo 注册中心也可以设置多个, 目前只拿第一个
+            String serverList = Arrays.stream(connect).map(Connect::connectString).collect(Collectors.joining(","));
+            properties.setProperty("serverAddr", serverList);
+        }
+        log.info("nacosServer start ...");
+        properties.setProperty("serverName", rcp.getNamespace());
+        client = NamingFactory.createNamingService(properties);
+        log.info(" ===> nacos client starting.");
     }
 
+    @SneakyThrows
     @Override
     public void stop() {
-
+        client.shutDown();
     }
 
     @SneakyThrows
     @Override
     public void register(ServiceMeta service, InstanceMeta instance) {
-        NamingService namingService = NamingFactory.createNamingService(service.toPath());
         Instance nacosInstance = new Instance();
+        nacosInstance.setServiceName(service.toPath());
         nacosInstance.setIp(instance.getHost());
         nacosInstance.setPort(instance.getPort());
-        nacosInstance.setMetadata(JSON.to(Map.class, instance.getGray()));
-        namingService.registerInstance(service.toMetas(), nacosInstance);
+        nacosInstance.setClusterName(instance.getContext());
+        nacosInstance.setMetadata(JSON.to(Map.class, JSON.toJSON(instance.getGray())));
+        nacosInstance.setHealthy(true);
+        client.registerInstance(service.toMetas(), nacosInstance);
+        log.info(" ===> register to nacos: {}", instance);
     }
 
     @SneakyThrows
     @Override
     public void unregister(ServiceMeta service, InstanceMeta instance) {
-        NamingService namingService = NamingFactory.createNamingService(service.toPath());
-        namingService.deregisterInstance(service.toMetas(), instance.getHost(), instance.getPort());
+        client.deregisterInstance(service.toPath(),
+                instance.getHost(),
+                instance.getPort(),
+                instance.getContext());
+        log.info(" ===> unregister from nacos: {}", instance);
     }
 
+    @SneakyThrows
     @Override
     public List<InstanceMeta> fetchAll(ServiceMeta service) {
-        return null;
+        List<Instance> nodes = client.selectInstances(service.toPath(), true);
+        return nodes.stream().map(instance -> {
+            InstanceMeta meta = InstanceMeta.http(
+                    instance.getIp(), instance.getPort(), instance.getClusterName());
+            GrayMetas grayMetas = JSON.to(GrayMetas.class, instance.getMetadata());
+            meta.setGray(grayMetas);
+            log.debug(" fetchAll instance: {}", meta.toUrl());
+            return meta;
+        }).collect(Collectors.toList());
     }
 
+    @SneakyThrows
     @Override
     public void subscribe(ServiceMeta service, ChangedListener changedListener) {
+
+        client.subscribe(service.toPath(), event -> {
+            // 节点变动，这里会感知到
+            log.info("nacos subscribe event: {}", event);
+            List<InstanceMeta> nodes = fetchAll(service);
+            changedListener.fire(new Event(nodes));
+        });
 
     }
 }
